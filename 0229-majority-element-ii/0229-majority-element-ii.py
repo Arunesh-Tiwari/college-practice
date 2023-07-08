5d366b0f80da857018567dc511876745e72e3f61
Arunesh Tiwari <77105301+Arunesh-Tiwari@users.noreply.github.com>
2023-07-08 12:55:10 +0530

    Time: 132 ms (48.24%), Space: 17.8 MB (71.11%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        dic=Counter(nums)
+        n,list1=len(nums),[]
+        for i,v in dic.items():
+            if v>n//3:
+                list1.append(i)
+        return list1
